f188cca8d5b4cc0d38a21caacc80f16c99451d0a
Rendra Kusuma Blikololo <rendrakusumab@gmail.com>
2025-04-16 19:36:03 +0700

    init project

---
+++ public/script/script.js
@@ -0,0 +1,161 @@
+// helper functions
+
+const scroll = (YCoor) => {
+    window.scrollTo({
+        top: YCoor,
+        behavior: "smooth",
+    });
+};
+
+// animation and interactivity for the website
+
+const header = document.querySelector("header");
+const desktopNav = document.querySelector(".desktop");
+const mobileNav = document.querySelector(".mobile");
+const burgerSVG = document.querySelector(".burger-menu-icon");
+const navLinks = document.querySelectorAll(".nav-links");
+const coordsForNav = {
+    homeCoor:
+        document.querySelector("#hero").getBoundingClientRect().top +
+        window.scrollY,
+    catalogCoor:
+        document.querySelector("#catalog").getBoundingClientRect().top +
+        window.scrollY,
+    testimoniesCoor:
+        document.querySelector("#testi").getBoundingClientRect().top +
+        window.scrollY,
+    contactCoor:
+        document.querySelector("#contact").getBoundingClientRect().top +
+        window.scrollY,
+    cart: 0,
+    profile: 0,
+};
+
+// assigning the nav links with the coords
+const coordsArray = Object.keys(coordsForNav);
+for (let i = 0; i < coordsArray.length; i++) {
+    navLinks[i].onclick = (e) => {
+        e.preventDefault();
+        scroll(coordsForNav[coordsArray[i]]);
+    };
+    navLinks[i + 6].onclick = (e) => {
+        e.preventDefault();
+        scroll(coordsForNav[coordsArray[i]]);
+    };
+}
+
+window.onscroll = () => {
+    if (window.scrollY > 50) {
+        header.classList.add("solidify");
+    } else {
+        header.classList.remove("solidify");
+    }
+};
+
+window.onload = () => {
+    if (window.innerWidth < 768) {
+        // make the header have actual height and not 0
+        header.style.height = "3.75rem";
+
+        burgerSVG.classList.remove("hidden");
+        desktopNav.classList.add("hidden");
+    }
+};
+
+burgerSVG.onclick = () => {
+    if (mobileNav.classList.contains("hidden")) {
+        setTimeout(() => {
+            mobileNav.classList.remove("hidden");
+        }, 100);
+    } else {
+        mobileNav.classList.add("hidden");
+    }
+
+    if (header.style.height === "3.75rem") {
+        header.style.height = "25rem";
+    } else {
+        header.style.height = "3.75rem";
+    }
+};
+
+// validate form
+
+const errorMessage = document.querySelector("#error-message-form");
+const form = document.querySelector("form");
+const button = document.querySelector("#form-button");
+
+button.onclick = (e) => {
+    errorMessage.classList.add("hidden");
+    e.preventDefault();
+
+    const elements = {
+        Nama: document.querySelector("#name-form"),
+        Email: document.querySelector("#email-form"),
+        Pesan: document.querySelector("#message-form"),
+    };
+
+    for (const key in elements) {
+        if (!elements[key].value) {
+            errorMessage.textContent =
+                `${key} tidak boleh kosong!`.toUpperCase();
+            errorMessage.classList.remove("hidden");
+            scroll(3600);
+            return;
+        }
+    }
+
+    form.reset();
+    window.alert("Pesan berhasil terkirim!");
+};
+
+// add to cart control
+
+const addToCartButtons = document.querySelectorAll(".add-to-cart");
+const quantityControl = document.querySelectorAll(".quantity-control");
+const confirmButtons = document.querySelectorAll(".confirm-button");
+const plus = document.querySelectorAll(".quantity-plus-inputs");
+const minus = document.querySelectorAll(".quantity-minus-inputs");
+const quantityLabels = document.querySelectorAll(".quantity-h3");
+let counter = 0;
+
+addToCartButtons.forEach((button, i) => {
+    button.onclick = () => {
+        if (!button.classList.contains("hidden")) {
+            button.classList.add("hidden");
+            quantityControl[i].classList.remove("hidden");
+        }
+    };
+});
+
+confirmButtons.forEach((button, i) => {
+    button.onclick = () => {
+        if (!button.classList.contains("hidden")) {
+            quantityControl[i].classList.add("hidden");
+            addToCartButtons[i].classList.remove("hidden");
+            counter = 0;
+            quantityLabels[i].textContent = counter;
+        }
+    };
+});
+
+plus.forEach((button, i) => {
+    button.onclick = () => {
+        counter++;
+        quantityLabels[i].textContent = counter;
+        if (counter > 99) {
+            counter = 99;
+            quantityLabels[i].textContent = counter;
+        }
+    };
+});
+
+minus.forEach((button, i) => {
+    button.onclick = () => {
+        counter--;
+        quantityLabels[i].textContent = counter;
+        if (counter < 0) {
+            counter = 0;
+            quantityLabels[i].textContent = counter;
+        }
+    };
+});
---
+++ resources/views/index.blade.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/style/style.css" />
+    <title>Markishop</title>
+  </head>
+  <body>
+    <!-- header & nav -->
+    <header>
+      <nav>
+        <ul class="desktop">
+          <li>
+            <a class="nav-links">Home</a>
+          </li>
+          <li>
+            <a class="nav-links">Catalog</a>
+          </li>
+          <li>
+            <a class="nav-links">Testimonies</a>
+          </li>
+          <li>
+            <a class="nav-links">Contact</a>
+          </li>
+          <li>
+            <a class="nav-links">Cart</a>
+          </li>
+          <li>
+            <a class="nav-links">Profile</a>
+          </li>
+        </ul>
+        <img
+          src="assets/burger-menu-svgrepo-com.svg"
+          alt="Menu"
+          class="hidden burger-menu-icon"
+        />
+        <ul class="mobile hidden">
+          <li>
+            <a class="nav-links">Home</a>
+          </li>
+          <li>
+            <a class="nav-links">Catalog</a>
+          </li>
+          <li>
+            <a class="nav-links">Testimonies</a>
+          </li>
+          <li>
+            <a class="nav-links">Contact</a>
+          </li>
+          <li>
+            <a class="nav-links">Cart</a>
+          </li>
+          <li>
+            <a class="nav-links">Profile</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <!-- hero -->
+      <section id="hero">
+        <h1>Markishop</h1>
+        <hr />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+          debitis odio animi? Omnis sit ut laborum, numquam, in libero hic,
+          ipsum mollitia qui fugiat nulla! Consectetur architecto porro
+          dignissimos aliquid in officia cumque saepe dicta dolore fuga, aliquam
+          voluptates. Odio sunt alias iusto laudantium nobis nulla, tempora
+          perferendis enim quisquam eaque? Hic a vero totam nulla id qui
+          obcaecati, ipsam similique voluptas? Doloribus repudiandae perferendis
+          obcaecati aliquid cupiditate fuga, commodi molestias optio
+          perspiciatis laudantium assumenda esse rerum hic omnis ad, nisi
+          similique fugiat eaque accusantium ea totam. Vero doloribus, quod
+          deserunt dignissimos voluptatem cum cupiditate quis eius
+          exercitationem nam sapiente!
+        </p>
+      </section>
+
+      <!-- catalog -->
+      <section id="catalog">
+        <h1>Catalog</h1>
+        <div id="products">
+          <article>
+            <img
+              src="https://images.pexels.com/photos/5760878/pexels-photo-5760878.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="product"
+              class="product-images"
+            />
+            <h3>Product Name</h3>
+            <p>$9.99</p>
+            <button class="add-to-cart">Add to Cart</button>
+            <div class="quantity-control hidden">
+              <button><h3 class="quantity-minus-inputs">-</h3></button>
+              <h3 class="quantity-h3">0</h3>
+              <button><h3 class="quantity-plus-inputs">+</h3></button
+              ><button class="confirm-button">Confirm</button>
+            </div>
+          </article>
+          <article>
+            <img
+              src="https://images.pexels.com/photos/5760878/pexels-photo-5760878.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="product"
+              class="product-images"
+            />
+            <h3>Product Name</h3>
+            <p>$9.99</p>
+            <button class="add-to-cart">Add to Cart</button>
+            <div class="quantity-control hidden">
+              <button><h3 class="quantity-minus-inputs">-</h3></button>
+              <h3 class="quantity-h3">0</h3>
+              <button><h3 class="quantity-plus-inputs">+</h3></button
+              ><button class="confirm-button">Confirm</button>
+            </div>
+          </article>
+          <article>
+            <img
+              src="https://images.pexels.com/photos/5760878/pexels-photo-5760878.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="product"
+              class="product-images"
+            />
+            <h3>Product Name</h3>
+            <p>$9.99</p>
+            <button class="add-to-cart">Add to Cart</button>
+            <div class="quantity-control hidden">
+              <button><h3 class="quantity-minus-inputs">-</h3></button>
+              <h3 class="quantity-h3">0</h3>
+              <button><h3 class="quantity-plus-inputs">+</h3></button
+              ><button class="confirm-button">Confirm</button>
+            </div>
+          </article>
+          <article>
+            <img
+              src="https://images.pexels.com/photos/5760878/pexels-photo-5760878.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="product"
+              class="product-images"
+            />
+            <h3>Product Name</h3>
+            <p>$9.99</p>
+            <button class="add-to-cart">Add to Cart</button>
+            <div class="quantity-control hidden">
+              <button><h3 class="quantity-minus-inputs">-</h3></button>
+              <h3 class="quantity-h3">0</h3>
+              <button><h3 class="quantity-plus-inputs">+</h3></button
+              ><button class="confirm-button">Confirm</button>
+            </div>
+          </article>
+          <article>
+            <img
+              src="https://images.pexels.com/photos/5760878/pexels-photo-5760878.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="product"
+              class="product-images"
+            />
+            <h3>Product Name</h3>
+            <p>$9.99</p>
+            <button class="add-to-cart">Add to Cart</button>
+            <div class="quantity-control hidden">
+              <button><h3 class="quantity-minus-inputs">-</h3></button>
+              <h3 class="quantity-h3">0</h3>
+              <button><h3 class="quantity-plus-inputs">+</h3></button
+              ><button class="confirm-button">Confirm</button>
+            </div>
+          </article>
+          <article>
+            <img
+              src="https://images.pexels.com/photos/5760878/pexels-photo-5760878.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="product"
+              class="product-images"
+            />
+            <h3>Product Name</h3>
+            <p>$9.99</p>
+            <button class="add-to-cart">Add to Cart</button>
+            <div class="quantity-control hidden">
+              <button><h3 class="quantity-minus-inputs">-</h3></button>
+              <h3 class="quantity-h3">0</h3>
+              <button><h3 class="quantity-plus-inputs">+</h3></button
+              ><button class="confirm-button">Confirm</button>
+            </div>
+          </article>
+        </div>
+      </section>
+
+      <!-- testi -->
+      <section id="testi">
+        <h1>Testimonies</h1>
+        <article>
+          <span>
+            <img
+              src="assets/user-3296.svg"
+              alt="user"
+              class="testimoni-icons"
+            />
+          </span>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            atque, consequuntur voluptate vel ipsum temporibus ea sapiente
+            minima dicta natus!
+          </p>
+        </article>
+        <article>
+          <span>
+            <img
+              src="assets/user-3296.svg"
+              alt="user"
+              class="testimoni-icons"
+            />
+          </span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            atque, consequuntur voluptate vel ipsum temporibus ea sapiente
+            minima dicta natus!
+          </p>
+        </article>
+        <article>
+          <span>
+            <img
+              src="assets/user-3296.svg"
+              alt="user"
+              class="testimoni-icons"
+            />
+          </span>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            atque, consequuntur voluptate vel ipsum temporibus ea sapiente
+            minima dicta natus!
+          </p>
+        </article>
+        <article>
+          <span>
+            <img
+              src="assets/user-3296.svg"
+              alt="user"
+              class="testimoni-icons"
+            />
+          </span>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            atque, consequuntur voluptate vel ipsum temporibus ea sapiente
+            minima dicta natus!
+          </p>
+        </article>
+      </section>
+
+      <!-- contact us -->
+      <section id="contact">
+        <h1>Contact us</h1>
+        <form>
+          <input type="text" placeholder="Name" id="name-form" />
+          <input type="email" placeholder="Email" id="email-form" />
+          <textarea placeholder="Message" id="message-form"></textarea>
+          <div>
+            <button id="form-button">Send</button>
+          </div>
+        </form>
+      </section>
+      <h2 id="error-message-form" class="hidden"></h2>
+    </main>
+
+    <!-- footer -->
+    <footer>
+      <p>&copy; 2024 Markishop</p>
+      <address>
+        <p>123 Market St</p>
+        <p>San Francisco, CA 94111</p>
+      </address>
+    </footer>
+
+    <script src="script/script.js"></script>
+  </body>
+</html>
