9c7367cf12a3a7d494291b7b5b13675e95322984
Rendra Kusuma Blikololo <rendrakusumab@gmail.com>
2025-04-22 18:40:53 +0700

    add testimony table

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Testimony;
 
 class ProductController extends Controller
 {
     public function index()
     {
         $products = Product::all();
+        $testimonies = Testimony::all();
 
-        return view('products.index', compact('products'));
+        return view('products.index', compact('products', 'testimonies'));
     }
 }
